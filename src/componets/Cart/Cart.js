c985bc642fb592cebbefa29e39be2ccf2ba11628
GianBaeza <gianb04@gmail.com>
2023-07-16 20:05:30 -0300

    icons

--- src/componets/Cart/Cart.js
+++ src/componets/Cart/Cart.js
@@ -17,9 +17,7 @@ const Cart = () => {
         return (
             <div className='container'>
                 <h1 className='containerH1'> No hay Productos en el carrito </h1>
-                <Link to='/' className='OptionP'> <span className="material-symbols-outlined">
-                    arrow_back
-                </span>  </Link>
+                <Link to='/' className='OptionP'> <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-left-circle"><circle cx="12" cy="12" r="10"/><path d="m14 16-4-4 4-4"/></svg>  </Link>
             </div>
         )
     }
