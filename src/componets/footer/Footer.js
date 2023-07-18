5c64e22614bce513fb68689191abbef1abc13b7c
GianBaeza <gianb04@gmail.com>
2023-07-18 20:41:06 -0300

    a

--- src/componets/footer/Footer.js
+++ src/componets/footer/Footer.js
@@ -14,7 +14,7 @@ export default function Footer() {
       <div className="SocialLinks">
         <Link to='https://www.facebook.com/'> <img className= 'a' src='https://i.postimg.cc/rwskSxwd/facebook-icon-icons-com-75091.png'/></Link>
         <Link to='https://twitter.com/home'> <img className= 'a' src='https://i.postimg.cc/7LbZYj2W/twitter.png'/></Link>
-        <Link to='https://www.instagram.com/t'> <img className= 'a' src='https://i.postimg.cc/J0312LfG/INSTAGRAM-icon-icons-com-65535.png'/></Link>
+        <Link to='https://www.instagram.com/'> <img className= 'a' src='https://i.postimg.cc/J0312LfG/INSTAGRAM-icon-icons-com-65535.png'/></Link>
         <Link to='https://github.com/Chi4n'> <img className= 'a' src='https://i.postimg.cc/GpsnfRYd/foldergreygit-93078.png'/></Link>
       
       </div>
